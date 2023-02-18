9971b62359344721494ac10f5b8cb310763c34ba
Bacemmallek999 <bacem.mallek@esprit.tn>
2023-02-18 17:58:18 +0100

    CRUD+ENTITIES

--- src/main/java/tn/esprit/spring/controllers/McqqestionController.java
+++ src/main/java/tn/esprit/spring/controllers/McqqestionController.java
@@ -2,7 +2,9 @@ package tn.esprit.spring.controllers;
 
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+import tn.esprit.spring.entity.Mcqqestion;
 import tn.esprit.spring.entity.Mcreponse;
+import tn.esprit.spring.services.IQuestionService;
 import tn.esprit.spring.services.IReponseService;
 
 import java.util.List;
@@ -11,30 +13,30 @@ import java.util.List;
 @AllArgsConstructor
 @RequestMapping("/mcqqestion")
 public class McqqestionController {
-    private final IQuestionService iReponseService;
+    private final IQuestionService iQuestionService;
     @PostMapping("/add")
     @ResponseBody
-    public Mcreponse addReponse(@RequestBody Mcreponse mcreponse){
-        return iReponseService.addReponse(mcreponse);
+    public Mcqqestion addQuestion(@RequestBody Mcqqestion mcqqestion){
+        return iQuestionService.addQuestion(mcqqestion);
     }
     @PutMapping("/update/{id}")
     @ResponseBody
-    public Mcreponse updateReponse(@RequestBody Mcreponse mcreponse, @PathVariable("id") Integer id){
-        return iReponseService.updateReponse(mcreponse,id);
+    public Mcqqestion updateQuestion(@RequestBody Mcqqestion mcqqestion, @PathVariable("id") Integer id){
+        return iQuestionService.updateQuestion(mcqqestion,id);
     }
     @DeleteMapping("/delete/{id}")
     @ResponseBody
-    public void deleteReponse(@PathVariable("id") Integer id){
-        iReponseService.deleteReponse(id);
+    public void deleteQuestion(@PathVariable("id") Integer id){
+        iQuestionService.deleteQuestion(id);
     }
     @GetMapping("/get/{id}")
     @ResponseBody
-    public Mcreponse getReponseById(@PathVariable("id") Integer id){
-        return iReponseService.retrieveReponseById(id);
+    public Mcqqestion getQuestionById(@PathVariable("id") Integer id){
+        return iQuestionService.retrieveQuestionById(id);
     }
     @GetMapping("/getall")
     @ResponseBody
-    public List<Mcreponse> getAllReponse(){
-        return iReponseService.retrieveAllReponses();
+    public List<Mcqqestion> getAllQuestion(){
+        return iQuestionService.retrieveAllQuestions();
     }
 }
