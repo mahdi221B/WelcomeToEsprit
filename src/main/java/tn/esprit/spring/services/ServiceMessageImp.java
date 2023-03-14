f96f4366032506e35a2069475fdb6f1780e657fa
mahdi221B <mahdi.dridi@esprit.tn>
2023-03-14 02:38:27 +0100

    Validation finalV1.1

--- src/main/java/tn/esprit/spring/services/ServiceMessageImp.java
+++ src/main/java/tn/esprit/spring/services/ServiceMessageImp.java
@@ -1,4 +1,5 @@
 package tn.esprit.spring.services;
+import edu.stanford.nlp.pipeline.Annotation;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -13,10 +14,13 @@ import tn.esprit.spring.repositories.UserRepository;
 import javax.transaction.Transactional;
 import java.time.Instant;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+
+import static tn.esprit.spring.services.BadWordFilterService.filterText;
 
 @Slf4j
 @Service
@@ -25,6 +29,7 @@ public class ServiceMessageImp implements IServiceMessage{
     private final MessageRepository messageRepository;
     private final ConversationRepository conversationRepository;
     private final UserRepository userRepository;
+    private TwilioService twilioService;
 
     @Override
     public List<Message> retrieveAllMessages() {
@@ -67,9 +72,18 @@ public class ServiceMessageImp implements IServiceMessage{
     public void sendMessageToConversatiob(Message message, Integer conversationId,Integer userId){
         Conversation conversation = conversationRepository.findById(conversationId).get();
         User user = userRepository.findById(userId).get();
-        message.setSender(user);
-        message.setConversation(conversation);
-        messageRepository.save(message);
+        if (filterText(message.getContent()).equals(message.getContent()) && message !=null) {
+            message.setSender(user);
+            message.setConversation(conversation);
+            messageRepository.save(message);
+        }
+        twilioService.sendSMS("Warning for Inappropriate Behavior on Forum Dear "+user.getFirstName()+"," +
+                "We are writing to inform you that your recent comment '"+message.getContent()+"' on our forum has been flagged for inappropriate " +
+                "language. Please note that such behavior is not tolerated on our platform and goes against our" +
+                " community guidelines. We kindly remind you to be respectful and considerate when engaging in " +
+                "discussions on our forum. Failure to adhere to our guidelines may result in a temporary or " +
+                "permanent ban.We appreciate your cooperation in ensuring a positive and safe environment for " +
+                "all members of our community.Best regards,ESPRIT UNIVERSITY");
     }
     public List<Message> getMessagesByUserAndConversationId(Integer userId, Integer conversationId){
         Conversation conversation = conversationRepository.findById(conversationId).orElseThrow(() -> new RuntimeException("Conversation not found"));
